227ec1257a10de28f6332fd29654eb38c49b7403
agsuns <agsuns@gmail.com>
2022-12-04 03:54:31 -0300

    AOC2022 feat: day 04

---
+++ day_04/part_01.js
@@ -0,0 +1,29 @@
+const { getInput } = require('../InputGrabber');
+
+const data = getInput();
+const linesArray = data.split(/\r\n|\n|\r/);
+
+const doesOneContainTheOther = (section1, section2) => {
+  if (section1[0] <= section2[0] && section1[1] >= section2[1]) return true;
+  if (section2[0] <= section1[0] && section2[1] >= section1[1]) return true;
+  return false;
+};
+
+const sectionMap = (line) => {
+  const sections = line.split(',');
+
+  let section1 = sections[0].split('-');
+  section1 = section1.map((item) => Number(item));
+  let section2 = sections[1].split('-');
+  section2 = section2.map((item) => Number(item));
+
+  return { section1, section2 };
+};
+
+const result = linesArray.reduce((acc, line) => {
+  const { section1, section2 } = sectionMap(line);
+  if (doesOneContainTheOther(section1, section2)) return acc + 1;
+  return acc;
+}, 0);
+
+console.log(result);
---
+++ day_04/part_02.js
@@ -0,0 +1,34 @@
+const { getInput } = require('../InputGrabber');
+
+const data = getInput();
+const linesArray = data.split(/\r\n|\n|\r/);
+
+const doTheyOverlap = (section1, section2) => {
+  const { biggestSec, smallestSec } =
+    section1[1] >= section2[1]
+      ? { biggestSec: section1, smallestSec: section2 }
+      : { biggestSec: section2, smallestSec: section1 };
+
+  if (smallestSec[1] <= biggestSec[1] && smallestSec[1] >= biggestSec[0])
+    return true;
+  return false;
+};
+
+const sectionMap = (line) => {
+  const sections = line.split(',');
+
+  let section1 = sections[0].split('-');
+  section1 = section1.map((item) => Number(item));
+  let section2 = sections[1].split('-');
+  section2 = section2.map((item) => Number(item));
+
+  return { section1, section2 };
+};
+
+const result = linesArray.reduce((acc, line) => {
+  const { section1, section2 } = sectionMap(line);
+  if (doTheyOverlap(section1, section2)) return acc + 1;
+  return acc;
+}, 0);
+
+console.log(result);
