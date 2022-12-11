20a928a67af3f0130f30e9b912f4d5a6c48e2db2
João Vitor Néias de Carvalho <agsuns@gmail.com>
2022-12-10 21:02:39 -0300

    AOC2022 feat: day 06

---
+++ day_06/part_01.js
@@ -0,0 +1,22 @@
+const { getInput } = require('../inputGetter');
+
+const datastream = getInput()
+
+let result = 0
+const subsetSize = 4
+
+const isSetUnique = (subset) => {
+  const set = new Set(subset)
+
+  if (set.size === subset.length) return true
+  return false
+}
+
+for(let i = 0; i < datastream.length - subsetSize; i++) {
+  if (isSetUnique(datastream.slice(i, i + subsetSize))) {
+    result = i + subsetSize
+    break
+  }
+}
+
+console.log(result)
